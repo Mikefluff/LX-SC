89681c0ffe25407ac45e233f8b81ed1c23963227
polycarpik <paladyina@gmail.com>
2017-04-21 11:57:04 +0300

    Adding tests and refactoring

--- contracts/UserFactory.sol
+++ contracts/UserFactory.sol
@@ -1,22 +1,27 @@
 pragma solidity 0.4.8;
 
 import './EventsHistoryUser.sol';
+import './UserLibrary.sol';
 import './UserProxy.sol';
 import './Storage.sol';
 import './Owned.sol';
 import './User.sol';
 
 contract UserFactory is EventsHistoryUser, Owned {
+    UserLibrary userLibrary;
+    // SkillLibrary skillLibrary;
 
-	event UserCreated(address indexed user, address proxy);
+	event UserCreated(address indexed user, address proxy, bytes32[] roles, bytes32[] skills);
 
-    function createUserWithProxyAndRecovery(Storage _storage, bytes32 _roles, bytes32 _skills) {
+    function createUserWithProxyAndRecovery(Storage _storage, bytes32[] _roles, bytes32[] _skills) {
         UserProxy proxy = new UserProxy();
         User user = new User(_storage, 'User');
         proxy.changeContractOwnership(user);
         user.claimContractOwnership();
         user.setUserProxy(proxy);
-        UserCreated(user, proxy);
+        _setRoles(user, _roles);
+        // _setSkills(user, _skills);
+        UserCreated(user, proxy, _roles, _skills);
     }
 
  	function setupEventsHistory(address _eventsHistory) onlyContractOwner() returns(bool) {
@@ -27,4 +32,31 @@ contract UserFactory is EventsHistoryUser, Owned {
         return true;
     }
 
+    function setupUserLibrary(UserLibrary _userLibrary) onlyContractOwner() returns(bool) {
+        userLibrary = _userLibrary;
+        return true;
+    }
+
+    // function setupSkillLibrary(SkillLibrary _skillLibrary) onlyContractOwner() returns(bool) {
+    //     skillLibrary = _skillLibrary;
+    //     return true;
+    // }
+
+    function _setRoles(address _user, bytes32[] _roles) internal returns(bool){
+        for(uint i = 0; i < _roles.length; i++){
+            userLibrary.addRole(_user, _roles[i]);
+            // if (!userLibrary.addRole(_user, _roles[i])){
+            //     return false;
+            // }
+        }
+        return true;
+    }
+
+
+    // function _setSkills(address _user, bytes32[] _skills) internal returns(bool){
+    //     for(uint i = 0; i < _skills.length; i++){
+    //         skillLibrary.addSkill(_user, _roles[i]);
+    //     }
+    //     return true;
+    // }
 }
--- test/userFactory.js
+++ test/userFactory.js
@@ -1,5 +1,7 @@
 const EventsHistory = artifacts.require('./EventsHistory.sol');
 const UserFactory = artifacts.require('./UserFactory.sol');
+const UserLibrary = artifacts.require('./UserLibrary.sol');
+const ManagerMock = artifacts.require('./ManagerMock.sol');
 const Storage = artifacts.require('./Storage.sol');
 const Reverter = require('./helpers/reverter');
 
@@ -8,7 +10,6 @@ contract('UserFactory', function(accounts) {
   afterEach('revert', reverter.revert);
 
   let userFactory;
-  let eventsHistory;
   let storage;
 
   before('setup', () => {
@@ -16,17 +17,27 @@ contract('UserFactory', function(accounts) {
     .then(instance => userFactory = instance)
     .then(() => Storage.deployed())
     .then(instance => storage = instance)
+    .then(() => ManagerMock.deployed())
+    .then(instance => storage.setManager(instance.address))
     .then(() => EventsHistory.deployed())
     .then(instance => eventsHistory = instance)
     .then(() => userFactory.setupEventsHistory(eventsHistory.address))
+    .then(() => UserLibrary.deployed())
+    .then(instance => userFactory.setupUserLibrary(instance.address))
     .then(reverter.snapshot);
   });
 
   it('should create users', () => {
-    return userFactory.createUserWithProxyAndRecovery(storage.address, 'newlyCreatedUser')
+    let roles = ['role1', 'role2'];
+    let skills = ['skill1', 'skill2'];
+    return userFactory.createUserWithProxyAndRecovery(storage.address, roles, skills)
     .then(result => {
       assert.equal(result.logs.length, 1);
       assert.equal(result.logs[0].event, 'UserCreated');
+      assert.equal(result.logs[0].roles, roles);
+      assert.equal(result.logs[0].skills, skills);
+      assert.notNull(result.logs[0].proxy);
+      assert.notNull(result.logs[0].user);
     });
   });
 
